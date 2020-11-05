5c0eb6b35457674ad51d622180e4c4d5a07037eb
Geno Ferollo <genemators@gmail.com>
2020-11-06 02:17:25 +0500

    updating names

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: '.NET Uzbekistan',
+  siteName: '.NET UZB',
   icon: {
     favicon: './src/assets/favicon.png',
     touchicon: './src/assets/favicon.png'
