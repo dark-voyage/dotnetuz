789c35919f74905787d0f4db18265cdb1050ebdb
Geno Ferollo <genemators@gmail.com>
2020-11-06 11:11:52 +0500

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,12 +2,12 @@
 	<Layout>
 		<div class="pt-8 md:pt-16">
 			<h1 class="text-center font-bold text-4xl text-ui-primary">
-				Oh no! There is nothing here.
+				Yopiram! Qovun tushirib qo'ydingizku!.
 			</h1>
 		</div>
 		<div class="mb-10 flex items-center justify-center">
 			<g-link to="/" class="font-bold border-b border-ui-primary text-xl"
-				>Go back</g-link
+				>To'girlash...</g-link
 			>.
 		</div>
 	</Layout>
