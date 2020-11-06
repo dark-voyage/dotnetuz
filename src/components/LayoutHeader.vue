6953bf9197bf4bc08a561cad3bc1e1dcc9145d30
Geno Ferollo <genemators@gmail.com>
2020-11-06 11:44:49 +0500

    Update LayoutHeader.vue

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -12,7 +12,7 @@
 					>
 						<Logo :width="40" class="text-ui-primary" />
 						<span
-							class="hidden ml-2 text-xl font-black uppercase sm:block"
+							class="hidden ml-2 text-xl font-black tracking-tighter uppercase sm:block"
 						>
 							{{ meta.siteName }}
 						</span>
